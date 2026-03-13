3a77ea27c99a141d037c770e197ad1d6903e8d8e
Keneth Ravindu <129732198+Keneth-Ravindu@users.noreply.github.com>
2026-03-13 08:32:15 +0530

    Delete app/views directory

--- app/views/layouts/footer.php
+++
@@ -1,6 +0,0 @@
-<script defer src="/Project-Visionary-Verse/public/assets/js/app.js"></script>
-<?php if (!empty($pageScript)): ?>
-    <script defer src="/Project-Visionary-Verse/public/assets/js/<?= $pageScript; ?>"></script>
-<?php endif; ?>
-</body>
-</html>
