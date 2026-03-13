343fbdf979e8999532f42b790240de006be5e006
Keneth Ravindu <129732198+Keneth-Ravindu@users.noreply.github.com>
2026-03-13 08:33:24 +0530

    Delete public/views directory

--- public/views/layouts/footer.php
+++
@@ -1,6 +0,0 @@
-<script defer src="/pvv/public/assets/js/app.js"></script>
-<?php if (!empty($pageScript)): ?>
-    <script defer src="/pvv/public/assets/js/<?= $pageScript; ?>"></script>
-<?php endif; ?>
-</body>
-</html>
